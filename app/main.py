6e211462f4d1f3eebc83640a2ecf8ec40ab16266
Jacques-DuToit <jacques_dutoit@outlook.com>
2025-01-03 15:36:37 +0000

    feat: function that creates a matchup table for types

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pandas.core.common import not_none
 
 
 def initialise_chart() -> pd.DataFrame:
@@ -192,26 +193,10 @@ def expand_chart(chart: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([chart, new_df], axis=1)
 
 
-def main():
-    chart = initialise_chart()
-    chart = expand_chart(chart)
-    chart.to_csv("chart.csv")
-
-    adv = (chart > 1)
-    dis = (chart < 1)
-    # Sum across the columns to find how many advantages they have
-    strong_against = adv.sum(axis=1).sort_values(ascending=False)
-    # Sum across the rows to find how many resistances they have
-    resistant_to = dis.sum(axis=0).sort_values(ascending=False)
-
-    find_matching(["fire", "fighting"], adv)
-
-
 def find_matching(input_types: list[str], adv: pd.DataFrame):
-    if len(input_types) < 1:
-        print("Need at least 1 input type.")
-        return
-
+    """
+    Finds in order the next types needed to be able to be super effective against the most types
+    """
     current = adv.copy()
     types = input_types.copy()
 
@@ -230,6 +215,83 @@ def find_matching(input_types: list[str], adv: pd.DataFrame):
         print(f"Remaining types not yet effective against: {current.shape[1]}")
 
     print(types)
+
+
+def matchup_generator(chart: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generates the matchups between all the different types in the form of a dataframe with columns
+    Type1, Type2, Res1, Res2, Str1, Str2, Res, Str
+
+    Where
+    - Resi is the resistance of Type1 to the ith type in Type2
+    - Stri is the strength of the ith type in Type1 against Type2
+    """
+    types = list(chart.columns)
+    rows = []
+    for i, type1 in enumerate(types):
+        for j, type2 in enumerate(types):
+            resist = [np.nan, np.nan]
+            strength = [np.nan, np.nan]
+
+            types_in_one = type1.split("/")
+            types_in_two = type2.split("/")
+
+            for t, two_type in enumerate(types_in_two):
+                resist[t] = chart.loc[two_type, type1]
+
+            for t, one_type in enumerate(types_in_one):
+                strength[t] = chart.loc[one_type, type2]
+
+            new_row = [type1, type2] + resist + strength
+            rows.append(new_row)
+
+    df = pd.DataFrame(columns=["Type1", "Type2", "Res1", "Res2", "Str1", "Str2"], data=rows)
+    df['Res'] = df[['Res1', 'Res2']].max(axis=1)
+    df['Str'] = df[['Str1', 'Str2']].max(axis=1)
+    return df
+
+
+def teams_of_six(chart: pd.DataFrame):
+    """
+    Generates the different teams of 6 from the different types you could have that cover all types
+    """
+    pass
+
+
+def triangle_finder(chart: pd.DataFrame):
+    """
+    Finds all the perfect type triangles such that each type has the same effectiveness against the next type, and
+    same resistance to the previous type
+    e.g.
+    - Type A -> x4 -> Type B -> x4 -> Type C -> x4 -> Type A
+    - Type A -> x0 -> Type C -> x0 -> Type B -> x0 -> Type A
+
+    For dual types - this must be true for both of their types
+    e.g.
+    - A/B, C/D, E/F
+    - (A -> x2 -> C/D) & (B -> x2 -> C/D)
+    """
+    
+
+def main():
+    chart = initialise_chart()
+    chart = expand_chart(chart)
+
+    chart.to_csv("chart.csv")
+
+
+    adv = (chart > 1)
+    dis = (chart < 1)
+    # Sum across the columns to find how many advantages they have
+    strong_against = adv.sum(axis=1).sort_values(ascending=False)
+    # Sum across the rows to find how many resistances they have
+    resistant_to = dis.sum(axis=0).sort_values(ascending=False)
+
+    # Sums up how effective things are against it - giving types that are overall the most resistant
+    general_resistance = chart.sum(axis=0).sort_values()
+    #print(general_resistance)
+
+    find_matching([], adv)
 
 
 if __name__ == '__main__':
