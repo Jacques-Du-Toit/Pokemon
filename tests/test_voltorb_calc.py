ac68edd148223b2b928e44730bef1f999a4004df
Jacques-DuToit <jacques_dutoit@outlook.com>
2025-01-20 15:44:54 +0000

    feat: re-design so that points and spaces don't change

--- tests/test_voltorb_calc.py
+++ tests/test_voltorb_calc.py
@@ -1,4 +1,4 @@
-from app.voltorb_sudoku_calc import find_possible_values, check_points_bombs
+from app.voltorb_sudoku_calc import find_possible_values, naive_checker, cancel_out_bombs_points
 
 
 def test_possible_values():
@@ -30,7 +30,7 @@ def test_check_points_bombs():
     cols = [
         [2, 3], [8, 2], [5, 1], [6, 1], [5, 1]
     ]
-    assert check_points_bombs(grid, rows, cols) == False
+    assert naive_checker(grid, rows, cols) == False
 
     grid = [
         ['B', '2', '1', '1', '1'],
@@ -39,4 +39,64 @@ def test_check_points_bombs():
         ['1', 'B', 'B', '1', 'B'],
         ['1', '3', '2', '1', '1']
     ]
-    assert check_points_bombs(grid, rows, cols) == False
+    assert naive_checker(grid, rows, cols) == False
+
+    # Test it removes bombs that if row has enough
+    grid = [
+        ['B', 'B', 'B/1/2', '1', 'B/1/2'],
+        ['B/1', 'B/1/2', 'B/1/2', 'B/1/2', 'B/1/2'],
+        ['B/1', 'B/1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', 'B/1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', 'B/1/2', 'B/1/2', 'B/1/2', 'B/1/2']
+    ]
+    target = [
+        ['B', 'B', '1/2', '1', '1/2'],
+        ['B/1', '1/2', 'B/1/2', 'B/1/2', 'B/1/2'],
+        ['B/1', '1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', '1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', '1/2', 'B/1/2', 'B/1/2', 'B/1/2']
+    ]
+    rows = [
+        [5, 2],
+        [4, 2],
+        [6, 1],
+        [5, 2],
+        [5, 1]
+    ]
+    cols = [
+        [3, 2], [7, 1], [4, 2], [7, 1], [4, 2]
+    ]
+    cancel_out_bombs_points(grid, rows, cols)
+    for r, row in enumerate(grid):
+        for c, val in enumerate(row):
+            assert set(val.split('/')) == set(target[r][c].split('/'))
+
+    # Test it removes bombs that if row has enough
+    grid = [
+        ['B', 'B', 'B/1/2', '1', 'B/1/2'],
+        ['B/1', 'B/1/2', 'B/1/2', 'B/1/2', 'B/1/2'],
+        ['B/1', 'B/1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', 'B/1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', 'B/1/2', 'B/1/2', 'B/1/2', 'B/1/2']
+    ]
+    target = [
+        ['B', 'B', '1/2', '1', '1/2'],
+        ['B/1', '1/2', 'B/1/2', 'B/1/2', 'B/1/2'],
+        ['B/1', '1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', '1/3/2', 'B/1/2', 'B/1/3/2', 'B/1/2'],
+        ['B/1', '1/2', 'B/1/2', 'B/1/2', 'B/1/2']
+    ]
+    rows = [
+        [5, 2],
+        [4, 2],
+        [6, 1],
+        [5, 2],
+        [5, 1]
+    ]
+    cols = [
+        [3, 2], [7, 1], [4, 2], [7, 1], [4, 2]
+    ]
+    naive_checker(grid, rows, cols)
+    for r, row in enumerate(grid):
+        for c, val in enumerate(row):
+            assert set(val.split('/')) == set(target[r][c].split('/'))
